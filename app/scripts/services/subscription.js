91e15d5fecd4c6a0770a25ca1c74106bb43e9a7a
Leonardo Celis <lei_to112@hotmail.com>
2015-05-24 19:40:34 -0500

    PP-04 Fix some errors and develop subscription and publish feature

--- app/scripts/services/subscription.js
+++ app/scripts/services/subscription.js
@@ -4,6 +4,33 @@ angular.module('promoPlatformApp')
 
 .factory('SubscriptionService',['$http','ApiEndpoint',function($http,ApiEndpoint){
     
+    
+    var getSubscription = function(manager_id){
+        return $http.get(ApiEndpoint.url + '/platform/managers/'+manager_id+'/subscriptions');
+    };
+    var publish = function(manager_id, product_id){
+        return $http.post(ApiEndpoint.url + '/platform/managers/'+manager_id+'/products/'+product_id+'/publish');
+    };
+    
+    
+
+    
+    var unPublish = function(manager_id, product_id){
+        return $http.delete(ApiEndpoint.url + '/platform/managers/'+manager_id+'/products/'+product_id+'/publish');
+    };
+    
+        var buySubscription = function(manager_id, subscription_id){
+        return $http.post(ApiEndpoint.url + '/platform/managers/'+manager_id+'/subscriptions',
+                          {subscription_id: subscription_id}
+                         );
+    };
+    
+    return {publish:publish,
+           unPublish:unPublish,
+           buySubscription:buySubscription,
+           getSubscription:getSubscription};
+    
+
 
 
 }]);
